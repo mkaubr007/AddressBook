f2a07d2d72fbf3301b5d60f5c577af3c92e4dcf8
mkaubr007 <mkaubr007@gmail.com>
2021-09-05 00:37:19 +0530

    edit contacts added

--- src/main/java/com/biz/address/book/AddressBook.java
+++ src/main/java/com/biz/address/book/AddressBook.java
@@ -1,6 +1,7 @@
 package com.biz.address.book;
 
 import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.Scanner;
 
 public class AddressBook extends Contacts {
@@ -30,10 +31,58 @@ public class AddressBook extends Contacts {
         String phoneNumber=s.next();
         contacts.setPhoneNumber(phoneNumber);
     }
+    public void edit() {
+        System.out.println("Enter your First name:");
+        String firstName = s.next();
 
+        Iterator<Contacts> iterator = contacts.listIterator();
+
+        while (iterator.hasNext()) {
+            Contacts contacts = iterator.next();
+
+            if (firstName.equals(contacts.getFirstName())) {
+                System.out.println("Choose field you want to add:");
+                System.out.println("1.Last Name\t2.Address\t3.City\t4.State\t5. Zip\6.Phone Number\7.Email");
+                switch (s.nextInt()) {
+                    case 1:
+                        System.out.println("Re-Correct your lastname");
+                        contacts.setLastName(s.next());
+                        break;
+                    case 2:
+                        System.out.println("Re-Correct your Address");
+                        contacts.setAddress(s.next());
+                        break;
+                    case 3:
+                        System.out.println("Re-Correct your City");
+                        contacts.setCity(s.next());
+                        break;
+                    case 4:
+                        System.out.println("Re-Correct your State");
+                        contacts.setState(s.next());
+                        break;
+                    case 5:
+                        System.out.println("Re-Correct your Zip");
+                        contacts.setZip(s.next());
+                        break;
+                    case 6:
+                        System.out.println("Re-Correct your Phone Number");
+                        contacts.setPhoneNumber(s.next());
+                        break;
+                    case 7:
+                        System.out.println("Re-Correct your Email");
+                        contacts.setEmail(s.next());
+                        break;
+                }
+
+            }
+        }
+
+    }
     public static void main(String[] args) {
         System.out.println("Welcome to Address Book");
         AddressBook addressBook=new AddressBook();
         addressBook.add();
+        System.out.println("enter first name to edit");
+        addressBook.edit();
     }
 }
