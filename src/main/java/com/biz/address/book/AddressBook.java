b51c177bc03de0a2fa52392334c1ffa1e4e32bb4
mkaubr007 <mkaubr007@gmail.com>
2021-09-11 16:25:26 +0530

    UniqueName added

--- src/main/java/com/biz/address/book/AddressBook.java
+++ src/main/java/com/biz/address/book/AddressBook.java
@@ -120,8 +120,6 @@ public class AddressBook  {
     public static void main(String[] args) {
         System.out.println("Welcome to Address Book");
         AddressBook addressBook=new AddressBook();
-        Contacts contacts=new Contacts();
         addressBook.operation();
-
     }
 }
