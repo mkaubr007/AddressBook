97f8099f871c8d709d2a3ea91bbee02be5b470bc
mkaubr007 <mkaubr007@gmail.com>
2021-09-11 16:24:15 +0530

    UniqueName added

--- src/main/java/com/biz/address/book/AddressBook.java
+++ src/main/java/com/biz/address/book/AddressBook.java
@@ -4,32 +4,36 @@ import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.Scanner;
 
-public class AddressBook extends Contacts {
+public class AddressBook  {
     Scanner s=new Scanner(System.in);
     ArrayList<Contacts> contacts=new ArrayList<Contacts>();
     public void add(){
         Contacts contacts=new Contacts();
-        System.out.println("enter first name");
-        String firstName=s.next();
-        contacts.setFirstName(firstName);
-        System.out.println("enter last name");
-        String lastName=s.next();
-        contacts.setLastName(lastName);
-        System.out.println("enter address ");
-        String address=s.next();
-        contacts.setAddress(address);
-        System.out.println("enter city");
-        String city=s.next();
-        contacts.setCity(city);
-        System.out.println("enter state");
-        String state=s.next();
-        contacts.setState(state);
-        System.out.println("enter zip");
-        String zip=s.next();
-        contacts.setZip(zip);
-        System.out.println("enter phone number");
-        String phoneNumber=s.next();
-        contacts.setPhoneNumber(phoneNumber);
+        if (!contacts.equals(contacts.getFirstName())) {
+            System.out.println("enter first name");
+            String firstName = s.next();
+            contacts.setFirstName(firstName);
+            System.out.println("enter last name");
+            String lastName = s.next();
+            contacts.setLastName(lastName);
+            System.out.println("enter address ");
+            String address = s.next();
+            contacts.setAddress(address);
+            System.out.println("enter city");
+            String city = s.next();
+            contacts.setCity(city);
+            System.out.println("enter state");
+            String state = s.next();
+            contacts.setState(state);
+            System.out.println("enter zip");
+            String zip = s.next();
+            contacts.setZip(zip);
+            System.out.println("enter phone number");
+            String phoneNumber = s.next();
+            contacts.setPhoneNumber(phoneNumber);
+        }else {
+            System.out.println("Duplicate contact");
+        }
     }
     public void edit() {
         System.out.println("Enter your First name:");
@@ -117,10 +121,7 @@ public class AddressBook extends Contacts {
         System.out.println("Welcome to Address Book");
         AddressBook addressBook=new AddressBook();
         Contacts contacts=new Contacts();
-        if (!contacts.equals(contacts.getFirstName())){
-            addressBook.operation();
-        }else {
-            System.out.println("Duplicate contact");
-        }
+        addressBook.operation();
+
     }
 }
